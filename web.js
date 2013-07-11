cc2ce2388b78bb8ca6002da84d5f7b5adf8a4083
Bakhytzhan Turekhanov <bturkbtu@gmail.com>
2013-07-11 05:47:55 +0000

    fixed buffer write in web.js 2

--- web.js
+++ web.js
@@ -3,10 +3,10 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 var buffer = new Buffer(16);
-buffer.write(fs.readFileSync(index.html));
+buffer.write("Kalai makali", "utf-8");
 
 app.get('/', function(request, response) {
-  response.send(buffer.toString());
+  response.send(buffer.toString("utf-8"));
 });
 
 var port = process.env.PORT || 5000;
