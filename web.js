65d391a6d09c04b7a0bcb47e2b7181d4b39f13db
Bakhytzhan Turekhanov <bturkbtu@gmail.com>
2013-07-12 05:05:33 +0000

    Updating index.html file 4

--- web.js
+++ web.js
@@ -3,11 +3,11 @@ var express = require('express');
 var fs=require("fs");
 var app = express.createServer(express.logger());
 
-var buffer = new Buffer(1024);
-buffer.write(fs.readFileSync("index.html", "utf-8"), "utf-8");
+var buffer = new Buffer(256);
+buffer.write(fs.readFileSync("index.html", "utf8"), "utf8");
 
 app.get('/', function(request, response) {
-  response.send(buffer.toString("utf-8"));
+  response.send(buffer.toString("utf8"));
 });
 
 var port = process.env.PORT || 5000;
